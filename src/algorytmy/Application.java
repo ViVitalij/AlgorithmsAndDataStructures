41305564214e70a2e68bec25672ccafcbc031e54
Vitalij <kontrabasista@wp.pl>
2017-02-14 18:08:49 +0100

    Initial commit

---
+++ src/algorytmy/Application.java
@@ -0,0 +1,10 @@
+package algorytmy;
+
+/**
+ * Created by m.losK on 2017-02-14.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Not to say hello world");
+    }
+}
