e854a861fd5b1e699caa7cbd24b8902c69f32a20
ViVitalij <kontrabasista@wp.pl>
2017-02-17 01:50:26 +0100

    Added set, addAll, clone and private checkIndex methods. Some changes in add, get and delete methods.

--- src/algorithms/structures/MyLinkedList.java
+++ src/algorithms/structures/MyLinkedList.java
@@ -25,17 +25,31 @@ public class MyLinkedList implements MyListInterface {
 
     @Override
     public void add(int index, int value) {
-
+        if (checkIndex(index)) {
+            MyLinkedList previousElement = getElement(index - 1);
+            MyLinkedList myLinkedList = getElement(index);
+            MyLinkedList addedElement = new MyLinkedList();
+            addedElement.value = value;
+            previousElement.next = addedElement;
+            addedElement.next = myLinkedList;
+        } else {
+            System.out.println("Wrong index!");
+        }
     }
 
     @Override
     public int get(int index) {
         MyLinkedList myLinkedList = getElement(index);
-        if (myLinkedList == null) {
-            System.out.println("Wrong index");
+        if (!checkIndex(index)) {
+            System.out.println("Wrong index!");
             return 0;
         }
         return myLinkedList.value;
+    }
+
+    private boolean checkIndex(int index) {
+        MyLinkedList myLinkedList = getElement(index);
+        return myLinkedList != null && index >= 0;
     }
 
     private MyLinkedList getElement(int index) {
@@ -51,35 +65,53 @@ public class MyLinkedList implements MyListInterface {
 
     @Override
     public void set(int index, int value) {
-
+        if (checkIndex(index)) {
+            MyLinkedList myLinkedList = getElement(index);
+            myLinkedList.value = value;
+        } else {
+            System.out.println("Wrong index!");
+        }
     }
 
     @Override
     public void addAll(MyListInterface myList) {
-
+        for (int i = 0; i < myList.getSize(); i++) {
+            this.add(myList.get(i));
+        }
     }
 
     @Override
     public void addAll(int index, MyListInterface myList) {
-
+        if (checkIndex(index)) {
+            for (int i = 0; i < myList.getSize(); i++) {
+                add(index + i, myList.get(i));
+            }
+        } else {
+            System.out.println("Wrong index!");
+        }
     }
 
     @Override
     public void delete(int index) {
-        if (index < getSize()) {
+        if (checkIndex(index)) {
             MyLinkedList previousElement = getElement(index - 1);
             MyLinkedList removableElement = getElement(index);
             previousElement.next = removableElement.next;
             removableElement.next = null;
+        } else {
+            System.out.println("Wrong index!");
         }
-
-
     }
 
 
     @Override
     public MyListInterface clone() {
-        return null;
+        MyLinkedList myLinkedList = this;
+        MyLinkedList tmpLinkedList = new MyLinkedList();
+        for (int i = 0; i < myLinkedList.getSize(); i++) {
+            tmpLinkedList.add(myLinkedList.get(i));
+        }
+        return tmpLinkedList;
     }
 
     @Override
