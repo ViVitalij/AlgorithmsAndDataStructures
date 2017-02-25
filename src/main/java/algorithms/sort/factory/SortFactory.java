a3b0aa7b31885f626dbf14bdcc86e10ed38ed1e9
Vitalij <kontrabasista@wp.pl>
2017-02-25 16:08:44 +0100

    Change SortFactory on design pattern - Singelton

--- src/main/java/algorithms/sort/factory/SortFactory.java
+++ src/main/java/algorithms/sort/factory/SortFactory.java
@@ -9,6 +9,13 @@ import algorithms.sort.impl.SelectionSort;
  * Created by m.losK on 2017-02-24.
  */
 public class SortFactory {
+    private static SortFactory instance = new SortFactory();
+    private SortFactory(){
+    }
+
+    public static SortFactory getInstance(){
+        return instance;
+    }
     public static Sort produce(Sort.sortEnum type) {
         if (Sort.sortEnum.BUBBLE.equals(type)) {
             return new BubbleSort();
